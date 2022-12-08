850d62c0313cae95390cb417efaf6c67a11c6757
OndrejVana <117227193+OndrejVana@users.noreply.github.com>
2022-12-08 11:22:54 +0100

    Add files via upload

--- AT_TINY.cpp
+++ AT_TINY.cpp
@@ -3,28 +3,47 @@
 #define DIR 1
 #define PUL 2
 
-int tol = 0, wait_time;
+#define S_speed 500 
+#define F_speed 5000
+#define kp 1
+
+int tol = 0, wait_time, Rot_dir = 1, d_speed = 0;
 double Target_angle, Real_angle, gain = 1, corretion = 0;
 
 void setup() {
 
 }
 
-void loop() {
-  while(true){
-    if(Target_angle > Real_angle + corretion or Target_angle > Real_angle - corretion){
+void Step(){
+  if(Target_angle > Real_angle){
+    Rot_dir = -1;
+  }
+  else{
+    Rot_dir = 1; 
+  }
+  int x = (abs(abs(Target_angle) - abs(Real_angle)) * kp);
+  wait_time = constrain(x, S_speed, F_speed);
+  
+  if(Rot_dir == 1){
       digitalWrite(DIR, HIGH);
       digitalWrite(PUL, HIGH);
       delay(wait_time);
       digitalWrite(PUL, LOW);
     }
-    else if (Target_angle < Real_angle + corretion or Target_angle < Real_angle - corretion){
+  else if (Rot_dir == -1){
       digitalWrite(DIR, LOW);
       digitalWrite(PUL, HIGH);
       delay(wait_time);
       digitalWrite(PUL, LOW);
     }
+
+}
+
+void loop() {
+  while(true){
+    
     {
+      Step();
       /* code */
     }
     
